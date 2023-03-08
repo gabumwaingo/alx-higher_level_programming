1113474d9c5b29d0d5e4f33673fccdb8cf415261
gabumwaingo <gabumwaingo@gmail.com>
2023-03-08 11:44:18 -0800

    Check if list has loops

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,20 +10,17 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *current, *check;
+	listint_t *current = list, *check = list;
 
-	if (list == NULL || list->next == NULL)
-		return (0);
-	current = list;
-	check = current->next;
-
-	while (current != NULL && check->next != NULL
-		&& check->next->next != NULL)
+	while (current && check && check->next)
 	{
-		if (current == check)
-			return (1);
 		current = current->next;
 		check = check->next->next;
+
+		if (current == check)
+		{
+			return (1);
+		}
 	}
 	return (0);
 }
