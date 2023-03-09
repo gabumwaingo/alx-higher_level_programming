794dac1cfdbea8382f383d4baeca654ea7aacf35
gabumwaingo <gabumwaingo@gmail.com>
2023-03-09 10:01:22 -0800

    Printing command line args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,5 +5,9 @@ if __name__ == "__main__":
     if n == 1:
         print(".")
     else:
+        if ((n - 1) == 1):
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(n - 1))
         for i in range(1, n):
             print("{0}: {1}".format(i, sys.argv[i]))
