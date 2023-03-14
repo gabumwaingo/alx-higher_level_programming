2048c1d91e1cb107a8452ffd620184e35681baa1
gabumwaingo <gabumwaingo@gmail.com>
2023-03-14 11:34:33 -0700

    Task 13s

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,35 +2,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * is_palindrome - checks if a linked list is a palindrome
- *
- * @head: the head address of the linked list
- *
- * Return: 1 if it's a palindrome, 0 else
- */
-
-int is_palindrome(listint_t **head)
+bool isPalindromeUtil(struct node** left,struct node* right)
 {
-	listint_t *cur = *head;
-	int tab[2048], i = 0, j = 0;
+    // Stop recursion when right
+    if (right == NULL)
+        return true;
 
-	if (*head)
-	{
-		while (cur)
-		{
-			tab[i] = cur->n;
-			cur = cur->next;
-			i++;
-		}
+    // If sub-list is not palindrome then
+    // no need to check for current left
+    // and right, return false
+    bool isp = isPalindromeUtil(left,
+                                right->next);
+    if (isp == false)
+        return false;
 
-		while (j < i / 2)
-		{
-			if (tab[j] == tab[i - j - 1])
-				j++;
-			else
-				return (0);
-		}
-	}
-	return (1);
+    // Check values at current left and right
+    bool isp1 = (right->data == (*left)->data);
+
+    // Move left to next node
+    *left = (*left)->next;
+
+    return isp1;
+}
+
+// A wrapper over isPalindromeUtil()
+bool isPalindrome(struct node* head)
+{
+    isPalindromeUtil(&head, head);
+}
+
+// Push a node to linked list.
+// Note that this function changes
+// the head
+void push(struct node** head_ref,
+          char new_data)
+{
+    // Allocate node
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    // Put in the data
+    new_node->data = new_data;
+
+    // Link the old list off the new node
+    new_node->next = (*head_ref);
+
+    // Move the head to point to the new node
+    (*head_ref) = new_node;
+}
+
+// A utility function to print a
+// given linked list
+void printList(struct node* ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%c->", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
 }
