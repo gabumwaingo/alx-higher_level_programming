d4a62380ddf1fa3924079ee3cab9eb32ad1593b6
gabumwaingo <gabumwaingo@gmail.com>
2023-04-12 19:40:58 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empty class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """Area public attribute"""
+        raise Exception("area() is not implemented")
