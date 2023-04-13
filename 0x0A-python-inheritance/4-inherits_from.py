9954977d3b85edc869e694b406a4212f23462152
gabumwaingo <gabumwaingo@gmail.com>
2023-04-12 19:14:43 -0700

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,8 @@
 Checks if obj is an instancr of a class that inherited
 directly or indeirectly from a_class
 """
+
+
 def inherits_from(obj, a_class):
     """Returns true if obj is from a_class and false if not"""
     return (issubclass(type(obj), a_class) and type(obj) != a_class)
