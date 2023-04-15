4e0f3fc2b8cfec29dcee79e3f7398fdfafbbdd07
gabumwaingo <gabumwaingo@gmail.com>
2023-04-15 10:28:56 -0700

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for class_to_json"""
+
+
+def class_to_json(obj):
+    """
+    A function that returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
