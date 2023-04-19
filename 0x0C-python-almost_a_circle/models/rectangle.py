bba61ee28944d56c5061225bbb9ab78ffb0c95bf
gabumwaingo <gabumwaingo@gmail.com>
2023-04-19 12:43:06 -0700

    Task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,6 +82,6 @@ class Rectangle(Base):
 
     def __str__(self):
         """Overriding the str method"""
-        return "[Rectangle] {} {}/{} - {}/{}".format(
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y,
                 self.__width, self.__height)
