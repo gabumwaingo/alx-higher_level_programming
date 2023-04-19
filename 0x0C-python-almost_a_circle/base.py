6616fe74020fa460113e4b973e2a7a9385f0a8c2
gabumwaingo <gabumwaingo@gmail.com>
2023-04-18 17:53:25 -0700

    Task 1

---
+++ 0x0C-python-almost_a_circle/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class Base"""
+
+
+import unittest
+class Base:
+    """Base class for all other classes in the project"""
+    __nb_objects = 0
+    """Private attribute"""
+
+    def __init__(self, id=None):
+        """Initialises the base class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test-0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Unit test module"""
+
+
+import unittest
+class Test(unittest.TestCase):
+
+    def Test_case1(self):
+        pass
+    def Test_case2(self):
+        pass
+    if __name__ == "__main__":
+    unittest.main()
