e82f7270ca0f58a02dcc3628bca874c1212e832c
gabumwaingo <gabumwaingo@gmail.com>
2023-04-19 13:30:04 -0700

    Task 8

--- 0x0C-python-almost_a_circle/rectangle.py
+++ 0x0C-python-almost_a_circle/rectangle.py
@@ -80,6 +80,26 @@ class Rectangle(Base):
             [print("#", end="") for w in range(self.width)]
             print("")
 
+    def update(self, *args):
+        """Assigns argument to each attribute"""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
     def __str__(self):
         """Overriding the str method"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
