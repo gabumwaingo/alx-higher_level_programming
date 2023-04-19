a7a00f437c4ed68827ea3fdfd01385b2c9f96776
gabumwaingo <gabumwaingo@gmail.com>
2023-04-19 14:05:58 -0700

    Task 10

---
+++ 0x0C-python-almost_a_circle/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialises the class"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        self.__width = value
+        self.__height = value
