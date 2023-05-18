9e7dc5b352cbe448f64e584aabe0ed22e093a565
gabumwaingo <gabumwaingo@gmail.com>
2023-05-18 12:49:16 -0700

    Chicago freestyle

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- creating a table first_table in sql
-CREATE IF TABLE NOT EXISTS `first_table` (
-	`id` INT,
-	`name` VARCHAR(256));
+CREATE IF TABLE NOT EXISTS mysql.first_table (
+	id INT,
+	name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- prints full description of first_table in hbtn_0c_0 database
+SHOW CREATE TABLE hbtn_0c_0.first_table
