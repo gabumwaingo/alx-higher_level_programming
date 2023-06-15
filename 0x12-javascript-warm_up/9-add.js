6bffba09b14d7603bd1a7ec61f73840aca379129
gabumwaingo <gabumwaingo@gmail.com>
2023-06-15 02:25:18 +0000

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,15 +3,4 @@ function add(a, b) {
   return a + b;
 }
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-const num1 = parseInt(arg1);
-const num2 = parseInt(arg2);
-
-if (Number.isNaN(num1) || Number.isNaN(num2)) {
-  console.log("Missing or invalid input. Please provide two integers.");
-} else {
-  const result = add(num1, num2);
-  console.log(result);
-}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
