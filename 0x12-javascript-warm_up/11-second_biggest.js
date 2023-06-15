1df981624d19be40db6cec9b43299cccdd8c0e76
gabumwaingo <gabumwaingo@gmail.com>
2023-06-15 08:41:17 +0000

    Task 12

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,11 +3,11 @@ const length = process.argv.length - 2;
 if (length === 0 || length === 1) {
   console.log(0);
 } else {
-  let largest = parseInt(process.argv[2]);
-  let secondLargest = parseInt(process.argv[2]);
+  let largest = (process.argv[2]);
+  let secondLargest = (process.argv[2]);
 
   for (let i = 3; i < process.argv.length; i++) {
-    let current = parseInt(process.argv[i]);
+    let current = (process.argv[i]);
 
     if (current > largest) {
       secondLargest = largest;
