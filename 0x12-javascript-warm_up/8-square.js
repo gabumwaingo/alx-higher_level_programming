389d77cf41fce8c20093e5b02422a0f9995a7166
gabumwaingo <gabumwaingo@gmail.com>
2023-06-15 01:55:07 +0000

    Task 9

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (Number.isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    let line = ' ';
+    let line = '';
     for (let j = 0; j < size; j++) {
       line += 'X';
     }
