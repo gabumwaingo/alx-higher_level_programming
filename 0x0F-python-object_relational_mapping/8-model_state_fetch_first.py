448866cd19a47e66c63e9ae62b116b839a09c14b
gabumwaingo <gabumwaingo@gmail.com>
2023-06-23 01:12:36 -0700

    Task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,7 +7,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """ Check if all arguments are provided """
-    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(          sys.argv[1], sys.argv[2], sys.argv[3])
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3])
     engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
