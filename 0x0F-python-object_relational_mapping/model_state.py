1ecd7031e1a263258569cf5ee4a4bc15e200de8c
gabumwaingo <gabumwaingo@gmail.com>
2023-06-22 18:49:27 -0700

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,13 +2,20 @@
 """ Creating orm abstraction with sql alchemy """
 
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
+    """State class
+    Attribute:
+        __table_name__ : name of the table
+        id : state id
+        name : names of the states
+
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
