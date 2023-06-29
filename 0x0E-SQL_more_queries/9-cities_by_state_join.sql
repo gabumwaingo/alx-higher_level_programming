72b4b3a1f2d8e8f157e0d9f6232907a2f33b2392
gabumwaingo <gabumwaingo@gmail.com>
2023-06-28 21:25:18 -0700

    Task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Listing all the cities in the database
-SELECT cities.id, cities.name, s.name
-FROM cities
-INNER JOIN states as s
-ON cities.state_id = s.id
-ORDER BY cities.id;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
